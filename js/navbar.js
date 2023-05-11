4152da433a6d263a10757d0d8cff3a632b7ffe77
Pham Duc Phuong <phuong.mom33@gmail.com>
2023-05-11 20:05:11 +0700

    ss

--- js/navbar.js
+++ js/navbar.js
@@ -1,2 +1,4 @@
 document.getElementById("btn__collapse").onclick = function () {document.getElementById("header__nav").classList.toggle("shownav");
 };
+document.getElementById("btn-search").onclick = function () {document.getElementById("inputsearch").classList.toggle("showinput");document.getElementById("btn-search").classList.toggle("formicon")
+};
